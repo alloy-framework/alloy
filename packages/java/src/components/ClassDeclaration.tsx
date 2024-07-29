281a05eddd4786638b661d5a62963a4309bb6524
M1lney <tomj.milne@gmail.com>
2024-07-29 23:03:57 +1200

    Update class declaration to take child component

--- packages/java/src/components/ClassDeclaration.tsx
+++ packages/java/src/components/ClassDeclaration.tsx
@@ -1,15 +1,17 @@
-import {code} from "@alloy-js/core";
+import {children, Children, code} from "@alloy-js/core";
 import {ClassConstructor} from "./ClassConstructor.js";
 
 export interface ClassDeclarationProps {
     accessModifier: string;
     className: string;
+    children?: Children;
 }
 
-export function ClassDeclaration({accessModifier, className}: ClassDeclarationProps ) {
+export function ClassDeclaration({accessModifier, className, children}: ClassDeclarationProps ) {
     return code`
                 ${accessModifier} class ${className} {
                     ${(<ClassConstructor accessModifier={accessModifier} className={className} />)}
+                    ${children}
                 }
     `
 }

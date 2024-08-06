80ba8896fed4e0edae2179492de199c9a104293a
M1lney <tomj.milne@gmail.com>
2024-08-06 17:02:37 +1200

    New sample output from java sample

--- packages/sample/sample-output/src/main/java/me/example/code/Main.java
+++ packages/sample/sample-output/src/main/java/me/example/code/Main.java
@@ -1,5 +1,10 @@
+package main.java.me.example.code;
+
+import main.java.me.example.code.models.Person;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    Person person = new Person("Tom", 27);
+    person.displayInfo();
   }
 }

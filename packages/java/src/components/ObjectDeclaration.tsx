bd07d3c13df8f79f71ad4a2ba9e9bd66011aea76
M1lney <tomj.milne@gmail.com>
2024-08-06 16:53:18 +1200

    Add comment

--- packages/java/src/components/ObjectDeclaration.tsx
+++ packages/java/src/components/ObjectDeclaration.tsx
@@ -3,7 +3,11 @@ import {DeclarationProps} from "./Declaration.js";
 import {Reference} from "./Reference.js";
 import {code, refkey} from "@alloy-js/core";
 
-
+/**
+ * I have seperated object and variable components. I thought this might be a good idea as I think this
+ * component will require more complex logic. Perhaps it should have a different name? I'm not sure if
+ * declaration is the correct word to describe it.
+ */
 interface ObjectDeclarationProps extends DeclarationProps{
     variableName: string;
     constructorArgs?: { value: any, type: 'string' | 'number' }[];

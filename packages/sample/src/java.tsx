086b5ce33e02cbd03685d192e8ff5d1ffc6bdab5
Jamin <jaminstratford@gmail.com>
2024-08-13 19:21:06 +1200

    update sample generator

--- packages/sample/src/java.tsx
+++ packages/sample/src/java.tsx
@@ -1,10 +1,11 @@
 import * as ay from "@alloy-js/core";
 import * as jv from "@alloy-js/java";
-import { AccessModifier, createJavaNamePolicy, createLibrary } from "@alloy-js/java";
+import { AccessModifier, createJavaNamePolicy, createLibrary, javaUtil } from "@alloy-js/java";
 import { writeOutput } from "./write-output.js";
+import { code, refkey } from "@alloy-js/core";
 
 const res = ay.render(
-  <ay.Output namePolicy={createJavaNamePolicy()}>
+  <ay.Output namePolicy={createJavaNamePolicy()} externals={[javaUtil]}>
     <jv.ProjectDirectory groupId="me.example" artifactId="test" version="1.0.0">
       <jv.PackageDirectory package="me.example.code">
         <jv.PackageDirectory package="enums">
@@ -61,6 +62,20 @@ const res = ay.render(
               return {ay.refkey("AnimalType")}.DOG;
             </jv.Method>
 
+          </jv.Class>
+        </jv.SourceFile>
+
+        <jv.SourceFile path="Main.java">
+          <jv.Class accessModifier='public' name="Main">
+            <jv.Constructor accessModifier='public' />
+
+            <jv.Method accessModifier='public' static name="main" parameters={{ args: "String[]"}}>
+              {code`
+                ${refkey(javaUtil['List'])}<${refkey("Animal")}> animals = new ${refkey(javaUtil['ArrayList'])}<>();
+                
+                animals.add(new ${refkey("Cat")}());
+              `}
+            </jv.Method>
           </jv.Class>
         </jv.SourceFile>
       </jv.PackageDirectory>

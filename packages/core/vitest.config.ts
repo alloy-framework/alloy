8d96fdc2ff6564fc6faf268fcabbd8fa347d1e5d
Timothee Guerin <tiguerin@microsoft.com>
2024-08-14 10:56:38 -0700

    Fix core tests

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
 import { babel } from "@rollup/plugin-babel";
+import { resolve } from "path";
 
+console.log("Config", resolve(__dirname, "./src/index.ts"));
 export default defineConfig({
   test: {
     include: ["test/**/*.ts", "test/**/*.tsx"],
@@ -9,6 +11,15 @@ export default defineConfig({
   esbuild: {
     jsx: "preserve",
     sourcemap: "both",
+  },
+  resolve: {
+    mainFields: ["module"],
+    alias: {
+      "#core": resolve(__dirname, "./src/index.ts"),
+      "@alloy-js/core/jsx-runtime": resolve(__dirname, "./src/jsx-runtime.ts"),
+      "@alloy-js/core/stc": resolve(__dirname, "./src/components/stc/index.ts"),
+      "@alloy-js/core": resolve(__dirname, "./src/index.ts"),
+    },
   },
   plugins: [
     babel({

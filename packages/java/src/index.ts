950330f5fc75928fe9d28ea1766cac83a13b5c93
Jamin <jaminstratford@gmail.com>
2024-08-16 19:57:39 +1200

    fix export

--- packages/java/src/index.ts
+++ packages/java/src/index.ts
@@ -3,4 +3,4 @@ export * from './name-policy.js';
 export * from './create-library.js';
 export * from './object-modifiers.js';
 export * from './arguments.js';
-export { createJavaSymbol } from "./symbols.js";
+export * from "./symbols.js";

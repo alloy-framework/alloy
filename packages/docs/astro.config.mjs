0c5014ee9baff5573a5157bbcd41d1265a049fa5
Brian Terlson <brian.terlson@microsoft.com>
2024-09-10 19:26:42 -0700

    Move readme contents to doc site

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,30 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Alloy',
-			social: {
-				github: 'https://github.com/alloy-framework/alloy',
-			},
-			plugins: [
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting started', slug: 'guides/getting-started' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Alloy",
+      social: {
+        github: "https://github.com/alloy-framework/alloy",
+      },
+      plugins: [],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Getting started", slug: "guides/getting-started" },
+            { label: "Basic concepts", slug: "guides/basic-concepts" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });

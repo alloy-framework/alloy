010db0ca49c2e73dc1fedaac7ad12434eeaaf18c
Jamin <jaminstratford@gmail.com>
2024-09-15 10:50:45 +1200

    add regression test for correct package directory #9

---
+++ packages/java/test/package.test.tsx
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+import * as jv from "../src/components/index.js";
+import { findFile, testRender } from "./utils.js";
+
+it("emits correct package directory", () => {
+  const res = testRender(
+    <>
+      <jv.PackageDirectory package='my.emit.package'>
+        <jv.SourceFile path="Test.java">
+        </jv.SourceFile>
+      </jv.PackageDirectory>
+    </>,
+  );
+
+  // Assert directory file is in is correct
+  const testFile = findFile(res, "Test.java");
+  expect(testFile.path).toBe("me/test/code/my/emit/package/Test.java");
+});

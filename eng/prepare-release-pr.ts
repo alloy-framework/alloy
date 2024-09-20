7e4cda63a8ec635b9cfea54e67d09110c1cb6dda
Timothee Guerin <tiguerin@microsoft.com>
2024-09-20 10:27:00 -0700

    Format

--- eng/prepare-release-pr.ts
+++ eng/prepare-release-pr.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 // @ts-check
 import { context, getOctokit } from "@actions/github";
-import { execSync } from "child_process";
 import {
   NodeChronusHost,
   renderReleasePlanAsMarkdown,
   resolveCurrentReleasePlan,
 } from "@chronus/chronus";
+import { execSync } from "child_process";
 const branchName = "publish/auto-release";
 
 const plan = await resolveCurrentReleasePlan(NodeChronusHost, process.cwd());
@@ -18,13 +18,17 @@ if (stdout.trim() !== "") {
   console.log("Commiting the following changes:\n", stdout);
 
   execSync(`git add -A`);
-  execSync(`git -c user.email=chronus@github.com -c user.name="Auto Chronus Bot" commit -am "Bump versions"`);
+  execSync(
+    `git -c user.email=chronus@github.com -c user.name="Auto Chronus Bot" commit -am "Bump versions"`,
+  );
   execSync(`git push origin HEAD:${branchName} --force`);
 
   console.log();
   console.log("-".repeat(160));
   console.log("|  Link to create the PR");
-  console.log(`|  https://github.com/timotheeguerin/chronus/pull/new/${branchName}  `);
+  console.log(
+    `|  https://github.com/timotheeguerin/chronus/pull/new/${branchName}  `,
+  );
   console.log("-".repeat(160));
 
   const github = getOctokit(process.env.GITHUB_TOKEN ?? "");

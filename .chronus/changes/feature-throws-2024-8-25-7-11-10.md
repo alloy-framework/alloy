a171d64e0d7c6b255cb1b7ed60c5fa27072bff80
Jamin <jaminstratford@gmail.com>
2024-09-26 07:49:45 +1200

    @alloy-js/java: Allowing specifying `throws` clause to methods (#37)

---
+++ .chronus/changes/feature-throws-2024-8-25-7-11-10.md
@@ -0,0 +1,7 @@
+---
+changeKind: feature
+packages:
+  - "@alloy-js/java"
+---
+
+@alloy-js/java: Allowing specifying `throws` clause to methods
--- packages/java/src/components/Method.tsx
+++ packages/java/src/components/Method.tsx
@@ -7,6 +7,7 @@ import { Parameters } from "./Parameters.js";
 export interface MethodProps extends ObjectModifiers, GenericTypes {
   name: string;
   return?: Children;
+  throws?: Children;
   parameters?: Record<string, Children>;
   children?: Children;
 }
@@ -15,6 +16,7 @@ export function Method(props: MethodProps) {
   const params = <Parameters parameters={props.parameters}></Parameters>;
   const name = useJavaNamePolicy().getName(props.name, "method");
   const modifiers = collectModifiers(props);
+  const throwsClause = props.throws ? code` throws ${props.throws}` : "";
   const generics = props.generics ?
     code`${collectGenerics(props.generics)}${" "}`
   : "";
@@ -26,6 +28,6 @@ export function Method(props: MethodProps) {
   `
   : ";";
   return code`
-        ${modifiers}${generics}${props.return ?? "void"} ${name}(${params})${sBody}
+        ${modifiers}${generics}${props.return ?? "void"} ${name}(${params})${throwsClause}${sBody}
     `;
 }

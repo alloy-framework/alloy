8ff513a41e4bcc95df96711dacba96005fa9ac85
Brian Terlson <brian.terlson@microsoft.com>
2025-02-16 13:06:29 -0800

    Add test

---
+++ packages/core/test/reactivity/memo.test.tsx
@@ -0,0 +1,18 @@
+import { memo } from "@alloy-js/core/jsx-runtime";
+import { ref } from "@vue/reactivity";
+import { expect, it } from "vitest";
+
+it("doesn't recalculate when dependencies don't change", () => {
+  const signal = ref(0);
+  let callCount = 0;
+  const m = memo(() => {
+    callCount += 1;
+    return signal.value;
+  });
+  console.log("first call");
+  expect(callCount).toBe(1);
+  m();
+  expect(callCount).toBe(1);
+  m();
+  expect(callCount).toBe(1);
+});

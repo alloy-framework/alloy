7922f110e4e8cb503131c54ecdf60e2747078c80
Brian Terlson <brian.terlson@microsoft.com>
2025-03-13 16:32:12 -0700

    Add if statement components

---
+++ packages/typescript/src/components/IfStatement.tsx
@@ -0,0 +1,51 @@
+import { Block } from "@alloy-js/core";
+import { Children } from "@alloy-js/core/jsx-runtime";
+
+export interface IfStatementProps {
+  condition: Children;
+  children: Children;
+}
+
+/**
+ * An if statement.
+ */
+export function IfStatement(props: IfStatementProps) {
+  return (
+    <>
+      if ({props.condition}) <Block>{props.children}</Block>
+    </>
+  );
+}
+
+export interface ElseIfClauseProps {
+  condition: Children;
+  children: Children;
+}
+
+/**
+ * An else clause with an if statement.
+ */
+export function ElseIfClause(props: ElseIfClauseProps) {
+  return (
+    <>
+      {" "}
+      else if ({props.condition}) <Block>{props.children}</Block>
+    </>
+  );
+}
+
+export interface ElseClauseProps {
+  children: Children;
+}
+
+/**
+ * The else clause of an if statement.
+ */
+export function ElseClause(props: ElseClauseProps) {
+  return (
+    <>
+      {" "}
+      else <Block>{props.children}</Block>
+    </>
+  );
+}

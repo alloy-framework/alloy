293c21a4932d12a173937709a5ad8fb07a2031d4
Brian Terlson <brian.terlson@microsoft.com>
2025-04-04 11:14:52 -0700

    fix private scope context docs

--- packages/typescript/src/context/private-scope.ts
+++ packages/typescript/src/context/private-scope.ts
@@ -1,12 +1,22 @@
-import { createContext, useContext } from "@alloy-js/core";
+import { ComponentContext, createContext, useContext } from "@alloy-js/core";
 import { TSMemberScope } from "../symbols/ts-member-scope.js";
 
 export interface PrivateScopeContext {
+  /**
+   * Scope for private static members.
+   */
   staticMembers: TSMemberScope;
+  /**
+   * Scope for private instance members.
+   */
   instanceMembers: TSMemberScope;
 }
 
-export const PrivateScopeContext = createContext<PrivateScopeContext>();
+/**
+ * Provides scopes for instance and static private members.
+ */
+export const PrivateScopeContext: ComponentContext<PrivateScopeContext> =
+  createContext<PrivateScopeContext>();
 
 export function usePrivateScope() {
   return useContext(PrivateScopeContext);

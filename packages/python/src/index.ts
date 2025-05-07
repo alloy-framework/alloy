ed886188d1af5fa77268c3e14d2419754e97b85e
swatikumar <swatikumar@pinterest.com>
2025-05-07 10:19:34 -0400

    Add a skeleton for python bindings

---
+++ packages/python/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Python bindings for Alloy
+ *
+ * This package provides components and utilities for generating Python code from Alloy.
+ * @packageDocumentation
+ */
+
+// Empty index file for now - components will be added as needed

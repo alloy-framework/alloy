094f3cf81dc7a01f38ea65ec34deaee1cbdd79a9
Timothee Guerin <tiguerin@microsoft.com>
2025-09-02 09:33:45 -0700

    Generate sourcemap alloy cli

--- packages/cli/src/babel.ts
+++ packages/cli/src/babel.ts
@@ -8,8 +8,12 @@ import alloyPreset from "@alloy-js/babel-preset";
 import { mkdir, writeFile } from "node:fs/promises";
 import { dirname, join, relative } from "pathe";
 
-export async function buildFile(filename: string) {
+export interface BuildOptions {
+  sourceMaps?: boolean;
+}
+export async function buildFile(filename: string, options: BuildOptions) {
   return babel.transformFileAsync(filename, {
+    sourceMaps: options.sourceMaps,
     presets: [typescriptPreset, alloyPreset],
   });
 }
@@ -18,14 +22,18 @@ export async function buildAllFiles(
   filenames: string[],
   rootDir: string,
   outDir: string,
+  options: BuildOptions = {},
 ) {
   await Promise.all(
     filenames.map(async (file) => {
-      const transform = await buildFile(file);
+      const transform = await buildFile(file, options);
       const relativePath = relative(rootDir, file).replace(/\.tsx?$/, ".js");
       const outPath = join(outDir, relativePath);
       await mkdir(dirname(outPath), { recursive: true });
       await writeFile(outPath, transform?.code as any);
+      if (transform?.map) {
+        await writeFile(outPath + ".map", JSON.stringify(transform.map));
+      }
     }),
   );
 }

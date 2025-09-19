ab658ddfb2be79ae57dc783751a6ab56375c37d5
Timothee Guerin <tiguerin@microsoft.com>
2025-09-19 12:44:19 -0700

    Simplify

--- packages/msbuild/src/components/index.test.tsx
+++ packages/msbuild/src/components/index.test.tsx
@@ -1,29 +1,16 @@
-import { describe, expect, it } from "vitest";
+import { expect, it } from "vitest";
 import { ItemGroup, PackageReference, PropertyGroup } from "./index.jsx";
 
-describe("ItemGroup", () => {
-  it("renders with children", () => {
-    expect(<ItemGroup>Content</ItemGroup>).toRenderTo(`
-        <ItemGroup>Content</ItemGroup>
-    `);
-  });
-  it("renders with label and children", () => {
-    expect(<ItemGroup Label="MyLabel">Content</ItemGroup>).toRenderTo(`
+it("renders ItemGroup", () => {
+  expect(<ItemGroup Label="MyLabel">Content</ItemGroup>).toRenderTo(`
         <ItemGroup Label="MyLabel">Content</ItemGroup>
     `);
-  });
 });
-describe("PropertyGroup", () => {
-  it("renders with children", () => {
-    expect(<PropertyGroup>Content</PropertyGroup>).toRenderTo(`
-        <PropertyGroup>Content</PropertyGroup>
-    `);
-  });
-  it("renders with label and children", () => {
-    expect(<PropertyGroup Label="MyLabel">Content</PropertyGroup>).toRenderTo(`
+
+it("renders PropertyGroup", () => {
+  expect(<PropertyGroup Label="MyLabel">Content</PropertyGroup>).toRenderTo(`
         <PropertyGroup Label="MyLabel">Content</PropertyGroup>
     `);
-  });
 });
 
 it("renders package reference", () => {

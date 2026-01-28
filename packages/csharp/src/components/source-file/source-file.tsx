ccdcdd7e19c1396d4d84086a06311841abd83297
Rodge Fu <wenjiefu@microsoft.com>
2026-01-28 17:50:13 +0800

    support doc comment for csharp src file (#354)

--- packages/csharp/src/components/source-file/source-file.tsx
+++ packages/csharp/src/components/source-file/source-file.tsx
@@ -1,3 +1,4 @@
+import { DocComment } from "#components/doc/comment.jsx";
 import { NamespaceScopes } from "#components/namespace-scopes.jsx";
 import { NamespaceName } from "#components/namespace/namespace-name.jsx";
 import { Reference } from "#components/Reference.jsx";
@@ -7,7 +8,9 @@ import {
   Children,
   computed,
   SourceFile as CoreSourceFile,
+  List,
   Scope,
+  Show,
   useBinder,
 } from "@alloy-js/core";
 import {
@@ -34,6 +37,10 @@ export interface SourceFileProps extends CSharpFormatOptions {
    * explicit usings is not necessary when referencing symbols via refkeys.
    */
   using?: string[];
+  /* * Optional header content to include at the top of the file */
+  header?: Children;
+  /* * Optional doc comment to include at the top of the file, this will be before 'header' if both are provided */
+  headerComment?: string;
 }
 
 /** A C# source file exists within the context of a namespace contains using statements and declarations */
@@ -59,12 +66,21 @@ export function SourceFile(props: SourceFileProps) {
     useTabs: props.useTabs,
   });
 
+  const header =
+    props.header || props.headerComment ?
+      <SourceFileHeader
+        header={props.header}
+        headerComment={props.headerComment}
+      />
+    : undefined;
+
   return (
     <CoreSourceFile
       path={props.path}
       filetype="cs"
       reference={Reference}
       {...opts}
+      header={header}
     >
       <Scope value={sourceFileScope}>
         {(sourceFileScope.usings.size > 0 ||
@@ -94,5 +110,22 @@ export function SourceFile(props: SourceFileProps) {
         }
       </Scope>
     </CoreSourceFile>
+  );
+}
+
+interface SourceFileHeaderProps {
+  header?: Children;
+  headerComment?: string;
+}
+
+function SourceFileHeader(props: SourceFileHeaderProps) {
+  return (
+    <List>
+      <Show when={props.headerComment !== undefined}>
+        <DocComment>{props.headerComment}</DocComment>
+      </Show>
+      <Show when={props.header !== undefined}>{props.header}</Show>
+      <hbr />
+    </List>
   );
 }

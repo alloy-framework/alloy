9e7289fd28eff41e67c045613421e140690b7b3c
RodgeFu <wenjiefu@microsoft.com>
2026-03-10 15:41:43 +0800

    update to use NamespaceScope

--- packages/csharp/src/components/namespace/namespace.tsx
+++ packages/csharp/src/components/namespace/namespace.tsx
@@ -6,40 +6,13 @@ import {
 } from "../../contexts/namespace.js";
 import { useSourceFileScope } from "../../scopes/source-file.js";
 import { createNamespaceSymbol } from "../../symbols/factories.js";
-import { NamespaceSymbol } from "../../symbols/namespace.js";
-import { NamespaceScope } from "../namespace-scopes.jsx";
+import { NamespaceScopes } from "../namespace-scopes.jsx";
 import { NamespaceName } from "./namespace-name.jsx";
 
 export interface NamespaceProps {
   name: string | Namekey | (string | Namekey)[];
   refkey?: Refkey | Refkey[];
   children?: Children;
-}
-
-/**
- * Wraps children with namespace scopes for each level of the namespace
- * hierarchy, stopping at the `stopAt` namespace symbol (which is already in
- * scope from an enclosing NamespaceScopes).
- */
-function wrapWithNamespaceScopes(
-  symbol: NamespaceSymbol,
-  children: Children,
-  stopAt?: NamespaceSymbol,
-): Children {
-  if (symbol === stopAt) {
-    return children;
-  }
-
-  const scopeChildren = (
-    <NamespaceScope symbol={symbol}>{children}</NamespaceScope>
-  );
-
-  const enclosing = symbol.enclosingNamespace;
-  if (enclosing && !enclosing.isGlobal && enclosing !== stopAt) {
-    return wrapWithNamespaceScopes(enclosing, scopeChildren, stopAt);
-  }
-
-  return scopeChildren;
 }
 
 export function Namespace(props: NamespaceProps) {
@@ -67,11 +40,9 @@ export function Namespace(props: NamespaceProps) {
           relative={!!hasOuterNamespace}
         />{" "}
         <Block>
-          {wrapWithNamespaceScopes(
-            namespaceSymbol,
-            props.children,
-            nsContext?.symbol,
-          )}
+          <NamespaceScopes symbol={namespaceSymbol} stopAt={nsContext?.symbol}>
+            {props.children}
+          </NamespaceScopes>
         </Block>
       </>
     );
